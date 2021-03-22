2c7233af1b6cca7fdb7b2547ca3739cb858f0d6f
helong <helong@qq.com>
2021-03-22 16:26:14 +0800

    Flutter: Provider

--- lib/pages/examples/example/home12.dart
+++ lib/pages/examples/example/home12.dart
@@ -2,9 +2,13 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 ///引入本地文件
 import '../../../tools/moon_extensions.dart';
 import '../../../tools/moon_size_extension.dart';
+import '../../../provider/Counter.dart';
+import '../../../provider/Config.dart';
 
 class TestHome12Page extends StatefulWidget {
   TestHome12Page({Key key, this.title, this.params}) : super(key: key);
@@ -41,7 +45,9 @@ class _TestHome12PageState extends State<TestHome12Page> {
 
   @override
   Widget build(BuildContext context) {
-    print("${widget.params},${widget.params['id']}");
+    // print("${widget.params},${widget.params['id']}");
+    print('home12 Widget build');
+    int _count = context.select((Counter counter) => counter.count); //你可以使用 context.select来 只监听count属性, 不能监听Map
 
     return Scaffold(
         appBar: AppBar(
@@ -68,21 +74,75 @@ class _TestHome12PageState extends State<TestHome12Page> {
             slivers: [
               SliverToBoxAdapter(
                 child: Container(
-                  height: 200,
-                  color: Colors.blue,
+                  width: 100,
+                  height: 30,
+                  color: Colors.red,
+                  child: RaisedButton.icon(
+                    icon: Icon(Icons.send),
+                    label: Text("改变姓名"),
+                    onPressed: () => {
+                      context.read<Counter>().modifyInfo(name: 'cloud-${new Random().nextInt(100)}')
+                    },
+                  ),
                 ),
               ),
-              // SliverAppBar(
-              //   title: Text('我是SliverAppBar'),
-              // ),
               SliverToBoxAdapter(
                 child: Container(
-                  height: 200,
+                  width: 100,
+                  height: 30,
                   color: Colors.red,
+                  child: RaisedButton.icon(
+                    icon: Icon(Icons.send),
+                    label: Text("改变年龄"),
+                    onPressed: () => {
+                      context.read<Counter>().modifyInfo(age: new Random().nextInt(100))
+                    },
+                  ),
                 ),
               ),
-              // SliverPadding(),
-              // SliverFixedExtentList(),
+              SliverToBoxAdapter(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.red,
+                  child: RaisedButton.icon(
+                    icon: Icon(Icons.send),
+                    label: Text("自增"),
+                    onPressed: () => {
+                      context.read<Counter>().increment()
+                    },
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  height: 30,
+                  color: Colors.red,
+                  child: Text(
+                    // '${Provider.of<Counter>(context).count}-${Provider.of<Counter>(context).sum}',
+                    '姓名: ${Provider.of<Counter>(context).info["name"]}'
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                    height: 30,
+                    color: Colors.blue,
+                    child: Text(
+                      // '${Provider.of<Counter>(context).count}'
+                      '年龄: ${context.read<Counter>().info["age"]}'
+                    )
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  height: 30,
+                  color: Colors.red,
+                  child: Text(
+                    'cuont: ${_count}'
+                  ),
+                ),
+              ),
             ]
         ),
         backgroundColor: ColorString("F5F6FA"));
--- lib/provider/Counter.dart
+++ lib/provider/Counter.dart
@@ -4,9 +4,14 @@ import 'package:flutter/foundation.dart';
 class Counter with ChangeNotifier, DiagnosticableTreeMixin {
   int _count = 0;
   int _sum = 0;
+  Map<String, dynamic> _info = {
+    "name": 'cloud',
+    "age": 28
+  };
 
   int get count => _count;
   int get sum => _sum;
+  get info => _info;
 
   increment() {
     _count++;
@@ -15,6 +20,20 @@ class Counter with ChangeNotifier, DiagnosticableTreeMixin {
 
   total(int x, int y) {
     _sum = x + y;
+    notifyListeners();
+  }
+
+  modifyInfo({ String name, int age }) {
+    // print('${name}-${age}');
+    if (name != null) {
+      _info["name"] = name;
+    }
+
+    if (age != null) {
+      _info["age"] = age;
+    }
+
+    // print(_info);
     notifyListeners();
   }
 
